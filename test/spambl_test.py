f9c3e4b95cb38f5aff5bad6692ac4fe469f5444d
Piotr Rusin <piotr.rusin88@gmail.com>
2015-11-10 23:13:23 +0100

    Add setUpClass method to DNSBLTest
    This method is used to set up a common instance of DNSBL for testing

--- test/spambl_test.py
+++ test/spambl_test.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from spambl import DNSBL
 
 
 class DNSBLTest(unittest.TestCase):
-    pass
-
-
-
+    
+    @classmethod
+    def setUpClass(cls):
+        code_item_class = {1: 'Class #1', 2: 'Class #2'}
+        query_suffix = 'query.suffix'
+        
+        cls.dnsbl = DNSBL('test.dnsbl', query_suffix, code_item_class, True, True)
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
