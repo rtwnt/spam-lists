cc13a93538c5babe253e77933fa288990c63ac41
Piotr Rusin <piotr.rusin88@gmail.com>
2015-11-10 23:02:45 +0100

    Add spambl_test module to test package, with DNSBLTest class

---
+++ test/spambl_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class DNSBLTest(unittest.TestCase):
+    pass
+
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
