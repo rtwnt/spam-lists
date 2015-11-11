951028cb76ab43a11095e8b8f47efde9742185de
Piotr Rusin <piotr.rusin88@gmail.com>
2015-11-11 18:32:58 +0100

    Add lists of spam hostnames, spam ips and hosts_with_spam object to spambl_test module
    These values can be shared by multiple testCase subclasses inside the module

--- test/spambl_test.py
+++ test/spambl_test.py
@@ -3,6 +3,15 @@
 
 import unittest
 from spambl import DNSBL, UnknownCodeError
+import mock
+from ipaddress import ip_address as IP
+
+spam_hostnames  = 't1.pl', 't2.com', 't3.com.pl'
+spam_ips = IP(u'255.255.0.1'), IP(u'2001:DB8:abc:123::42')
+
+hosts_with_spam = mock.Mock()
+hosts_with_spam.ips = spam_ips + (IP(u'127.180.0.18'),)
+hosts_with_spam.hostnames = spam_hostnames + ('valid.com',)
 
 
 class DNSBLTest(unittest.TestCase):
