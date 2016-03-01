7922a31953e92298c7e719d0262c8c045953431d
Piotr Rusin <piotr.rusin88@gmail.com>
2016-03-01 19:22:13 +0100

    Add BaseValueTesterTest class

--- test/base_test_cases.py
+++ test/base_test_cases.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+from spambl import AddressListItem
+
+class BaseValueTesterTest(object):
+    
+    classification = ('TEST',)
+    
+    def _get_expected_items(self, values):
+        item = lambda i: AddressListItem(i, self.tested_instance,
+                                             self.classification)
+        return [item(v) for v in values]
+        
