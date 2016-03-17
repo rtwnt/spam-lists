13774603c40cb5d09ebfa3c88c94f49156fedd68
Piotr Rusin <piotr.rusin88@gmail.com>
2016-03-17 08:05:15 +0100

    Add clients module

---
+++ spam_lists/clients.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+'''
+This module contains instances of objects representing clients of services
+that can be ready for use without providing custom, user-specific data, like
+API codes, application identifiers or custom data sets.
+'''
