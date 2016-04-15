05cc92149d2c87bc9074eb08b08fa0bff7dc18ec
Piotr Rusin <piotr.rusin88@gmail.com>
2016-04-15 22:28:57 +0200

    Fix indentation in function definition

--- spam_lists/structures.py
+++ spam_lists/structures.py
@@ -200,27 +200,27 @@ class IPv6Address(ipaddress.IPv6Address, IPAddress):
 
 
 def create_host(factories, value):
-        ''' Create an instance of host object for given value, using
-        the factories.
+    ''' Create an instance of host object for given value, using
+    the factories.
 
-        :param factories: a list of functions that return host objects
-        (Hostname, IPv4Address, IPv6Address) for valid arguments
-        :param value: a value to be passed as argument to factories
-        :returns: an object representing value, created by one of the factories.
-        It's a return value of the first factory that could create it
-        for the given argument
-        :raises InvalidHostError: if the value is not a valid input
-        for any factory used by this function
-        '''
-        data = [value]
-        for f in factories:
-            try:
-                return  f(value)
-            except InvalidHostError as e:
-                data.append(str(e))
-        msg_tpl = "Failed to create a host object for '{}', \
-        raising the following errors in the process:"+"\n".join(data)
-        raise InvalidHostError(msg_tpl.format(value))
+    :param factories: a list of functions that return host objects
+    (Hostname, IPv4Address, IPv6Address) for valid arguments
+    :param value: a value to be passed as argument to factories
+    :returns: an object representing value, created by one of the factories.
+    It's a return value of the first factory that could create it
+    for the given argument
+    :raises InvalidHostError: if the value is not a valid input
+    for any factory used by this function
+    '''
+    data = [value]
+    for f in factories:
+        try:
+            return  f(value)
+        except InvalidHostError as e:
+            data.append(str(e))
+    msg_tpl = "Failed to create a host object for '{}', \
+    raising the following errors in the process:"+"\n".join(data)
+    raise InvalidHostError(msg_tpl.format(value))
 
 def ip_address(value):
     ''' Create an ip address object
