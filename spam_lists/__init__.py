12f2eaad8cb396a716590f965d16b18c6f1a6c83
Piotr Rusin <piotr.rusin88@gmail.com>
2016-04-18 09:10:15 +0200

    Add imports to spam_lists.__init__

--- spam_lists/__init__.py
+++ spam_lists/__init__.py
@@ -1,1 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .clients import SPAMHAUS_DBL, SPAMHAUS_ZEN, SURBL_MULTI
+from .service_models import HpHosts, GoogleSafeBrowsing, HostCollection
+from .utils import UrlTesterChain, GeneralizedUrlTester
