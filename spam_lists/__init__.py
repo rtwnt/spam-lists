9b0b05fb8cb890701482c7758a037e78ed61a106
Piotr Rusin <piotr.rusin88@gmail.com>
2016-04-18 09:10:56 +0200

    Add package attributes to spam_lists.__init__

--- spam_lists/__init__.py
+++ spam_lists/__init__.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from .clients import SPAMHAUS_DBL, SPAMHAUS_ZEN, SURBL_MULTI
 from .service_models import HpHosts, GoogleSafeBrowsing, HostCollection
 from .utils import UrlTesterChain, GeneralizedUrlTester
+
+__title__ = 'spam-lists'
+__version__ = '0.9'
+__author__ = 'Piotr Rusin'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Piotr Rusin'
