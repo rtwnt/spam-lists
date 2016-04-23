e0f1ceafc1907de2ad0a8b40a12382d7d568411b
Piotr Rusin <pior.rusin88@gmail.com>
2016-04-23 13:14:50 +0200

    Refactor test.compat module
    The refactoring includes reordering of imports, removing unnecessary tags
    and handling alternative imports with exceptions rather than
    conditional structures.

--- test/compat.py
+++ test/compat.py
@@ -3,27 +3,31 @@
 A common module for compatibility related imports and
 definitions used during testing
 '''
+# pylint: disable=unused-import
 
 from __future__ import unicode_literals
 
 import unittest
 
-from six import assertCountEqual, PY2, PY3
-
+try:
+    from functools import lru_cache  # @NoMove
+except ImportError:
+    # pylint: disable=import-error
+    from cachetools.func import lru_cache  # @NoMove @UnusedImport
 
 try:
-    from unittest.mock import Mock, MagicMock, patch # @NoMove
+    from unittest.mock import Mock, MagicMock, patch  # @NoMove
 except ImportError:
+    # pylint: disable=import-error
     from mock import Mock, MagicMock, patch  # @NoMove @UnusedImport
 
-if PY3:
-    from functools import lru_cache # @NoMove @UnusedImport @UnresolvedImport
+from six import assertCountEqual, PY2
+
 
 class Py2TestCase(unittest.TestCase):
     def assertCountEqual(self, expected_sequence, actual_sequence):
         return assertCountEqual(self, expected_sequence, actual_sequence)
-        
+
+
 if PY2:
     unittest.TestCase = Py2TestCase
-    from cachetools.func import lru_cache # @NoMove @UnusedImport @Reimport @UnresolvedImport
-    
